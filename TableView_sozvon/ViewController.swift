e02e345d81f711b5aab5da0344caddbb1e06d385
Arthur Sh <arthur.shemshur@gmail.com>
2024-01-29 16:56:54 +0200

    Initial Commit

---
+++ TableView_sozvon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_sozvon
+//
+//  Created by Arthur Sh on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
